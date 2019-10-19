5b639988279c6be6cab352086bae8ee4922c0c3d
xjdlb <xjd.binlee@qq.com>
2019-10-19 21:36:15 +0800

    [add]增加了config需求.

--- demo-common/src/main/java/com/hz/democommon/config/ConfigHelper.java
+++ demo-common/src/main/java/com/hz/democommon/config/ConfigHelper.java
@@ -8,5 +8,10 @@ package com.hz.democommon.config;
 public class ConfigHelper {
 
     //todo这里写读取各自子模块下以dome-xxx.properties的配置文件,并加载到内存，同时获取文件名
+    //java运行的系统：win linux
+    //java运行的模式：idea-debug,win-jar,linux-jar
+    //配置文件异常在内部处理
+    //如何识别模块
+    //如何识别classpath路径
 
 }
