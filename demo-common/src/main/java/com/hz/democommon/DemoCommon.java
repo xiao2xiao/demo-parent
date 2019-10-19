bb9beda61378339242a8bef46bdbe828d62661a9
xjdlb <xjd.binlee@qq.com>
2019-10-19 21:07:39 +0800

    [add]新建了common模块，放一些公用的工具和实体类.

---
+++ demo-common/src/main/java/com/hz/democommon/DemoCommon.java
@@ -0,0 +1,16 @@
+package com.hz.democommon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: leebin
+ * @Date: 2019-10-19 21:01
+ * @Description:
+ */
+@SpringBootApplication()
+public class DemoCommon {
+    public static void main(String[] args) {
+        SpringApplication.run(DemoCommon.class, args);
+    }
+}
