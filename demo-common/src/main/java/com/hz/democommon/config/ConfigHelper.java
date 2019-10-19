02d16f3b9deddf719fedd0b7af69a1205f8c3da4
xjdlb <xjd.binlee@qq.com>
2019-10-19 21:23:17 +0800

    [add]增加了config需求.

---
+++ demo-common/src/main/java/com/hz/democommon/config/ConfigHelper.java
@@ -0,0 +1,12 @@
+package com.hz.democommon.config;
+
+/**
+ * @Author: leebin
+ * @Date: 2019-10-19 21:21
+ * @Description:
+ */
+public class ConfigHelper {
+
+    //todo这里写读取各自子模块下以dome-xxx.properties的配置文件,并加载到内存，同时获取文件名
+
+}
